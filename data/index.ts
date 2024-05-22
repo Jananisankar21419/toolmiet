1b548c63e7d01718130c5748ed200365e34dc50a
Janani <75440572+Jananisankar21419@users.noreply.github.com>
2024-05-22 22:58:50 +0530

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -4,16 +4,14 @@ export const projects = [
     title: "Framer Motion",
     des: "A production-ready animation library for React",
     img: "/framer.png",
-
     link: "https://www.framer.com",
-    onClick:"https://www.framer.com"
+    onClick: "https://www.framer.com"
   },
   {
     id: 2,
     title: "GSAP",
     des: "Professional-grade animation library for the modern web.",
     img: "/gsap.png",
-
     link: "https://gsap.com",
   },
   {
@@ -21,7 +19,6 @@ export const projects = [
     title: "Spline - 3D Design Tool",
     des: "A 3D design tool useful for the web-application",
     img: "spline.png",
-
     link: "https://spline.design/",
   },
   {
@@ -29,7 +26,6 @@ export const projects = [
     title: "Ibelick Animations",
     des: "A curated collection of animations crafted exclusively with Tailwind CSS",
     img: "/ibelick.png",
-
     link: "https://animation.ibelick.com/",
   },
   {
@@ -37,7 +33,6 @@ export const projects = [
     title: "Hover.dev",
     des: "Animated UI Components for React & Tailwind",
     img: "/hover.png",
-
     link: "https://www.hover.dev/",
   },
   {
@@ -45,7 +40,6 @@ export const projects = [
     title: "Aceternity UI",
     des: "Copy paste the most trending components and use them in your websites without having to worry about styling and animations.",
     img: "/aceternity.png",
-
     link: "https://ui.aceternity.com/",
   },
   {
@@ -53,23 +47,20 @@ export const projects = [
     title: "App Backend",
     des: "A simple backend for your static website to make it dynamic.",
     img: "appbackend.png",
-
     link: "https://appbackend.io/",
   },
-   {
+  {
     id: 8,
     title: "Postman",
     des: "Postman is an API platform for building and using APIs. Create better APIs - faster.",
     img: "postman.png",
-
     link: "https://www.postman.com/",
   },
-   {
+  {
     id: 9,
     title: "Replicate",
     des: "AI API for generating synthetic data. Run and fine-tune open-source models.",
     img: "replicate.png",
-
     link: "https://replicate.com/",
   },
   {
@@ -77,7 +68,6 @@ export const projects = [
     title: "Revert",
     des: "Revert is an open-source unified API to build, maintain and scale your ALL of your products",
     img: "revert.png",
-
     link: "https://www.revert.dev/",
   },
   {
@@ -85,7 +75,6 @@ export const projects = [
     title: "Strapi",
     des: "The leading open-source headless CMS integration with your apps",
     img: "strapi.png",
-
     link: "https://strapi.io/",
   },
   {
@@ -93,23 +82,13 @@ export const projects = [
     title: "UploadThing",
     des: "A simple file uploader for your web app especially for Nextjs Developers",
     img: "upload.png",
-
     link: "https://uploadthing.com/",
   },
-   {
-    id: 12,
-    title: "UploadThing",
-    des: "A simple file uploader for your web app especially for Nextjs Developers",
-    img: "upload.png",
-
-    link: "https://uploadthing.com/",
-  },
-   {
+  {
     id: 13,
     title: "Adobe Fonts",
     des: "Adobe Fonts partners with the world’s leading type foundries to bring thousands of fonts to designers every day.",
     img: "adobe.png",
-
     link: "https://fonts.adobe.com/",
   },
   {
@@ -117,15 +96,13 @@ export const projects = [
     title: "Art Directed Mockups",
     des: "Art Directed is a design resources company that creates thoughtful designs",
     img: "art.png",
-
     link: "https://artdirected.design/",
   },
   {
-    id:15,
+    id: 15,
     title: "Atipo Foundry",
     des: "An independent digital typeface foundry and graphic design studio.",
     img: "atipo.png",
-
     link: "https://www.atipofoundry.com/",
   },
   {
@@ -133,7 +110,6 @@ export const projects = [
     title: "Awwwards",
     des: "Awwwards showcase and award websites that demonstrate exceptional design, innovation, and user experience.",
     img: "awards.png",
-
     link: "https://www.awwwards.com/",
   },
   {
@@ -141,7 +117,6 @@ export const projects = [
     title: "Blender",
     des: "Blender is a free and open-source 3D computer graphics software tool set used for creating animated films, visual effects",
     img: "blender.png",
-
     link: "https://www.blender.org/",
   },
   {
@@ -149,7 +124,6 @@ export const projects = [
     title: "Codrops",
     des: "Keeping web professionals inspired & up-to-date since 2009",
     img: "codrops.png",
-
     link: "https://tympanus.net/codrops/",
   },
   {
@@ -157,7 +131,6 @@ export const projects = [
     title: "Curated.design",
     des: "Unleash your creativity with out-of-this-world web design inspiration from across the web",
     img: "curated.png",
-
     link: "https://www.curated.design/",
   },
   {
@@ -165,71 +138,62 @@ export const projects = [
     title: "Dark Mode Design",
     des: "Dark Mode Design showcases beautifully designed and inspiring dark mode websites. ",
     img: "darkmode.png",
-
     link: "https://www.darkmodedesign.com/",
   },
   {
-    id:21,
+    id: 21,
     title: "Doing Cool Stuff",
     des: "A directory of brand, motion and digital product design studios doing cool stuff.",
     img: "dcs.png",
-
     link: "https://www.doingcoolstuff.xyz/",
   },
   {
-    id:22,
+    id: 22,
     title: "Easings",
     des: "Easing functions specify the rate of change of a parameter over time.",
     img: "easings.png",
-
     link: "https://easings.net/",
   },
   {
-    id:23,
+    id: 23,
     title: "Fontshare",
     des: "Fontshare is a free fonts service from the Indian Type Foundry (ITF), making quality fonts accessible to all.",
     img: "fontshare.png",
-
     link: "https://www.fontshare.com/",
   },
   {
-    id:24,
+    id: 24,
     title: "Footer.design",
     des: "Footer is a curated gallery of the top website footer inspiration on earth. Find the footers you need and sort by type and style.",
     img: "footerdesign.png",
-
     link: "https://www.footer.design/",
   },
   {
-    id:25,
+    id: 25,
     title: "Frontend Mentor",
     des: "Improve your front-end skills by building real projects. Solve real-world HTML, CSS and JavaScript challenges ",
     img: "frontend.png",
-
     link: "https://www.frontendmentor.io/",
   },
   {
-    id:26,
+    id: 26,
     title: "Frontend Practice",
     des: "Take your frontend skills to the next level by recreating real websites from real companies.",
     img: "frontend-practice",
-
     link: "https://www.frontendpractice.com/",
   },
   {
-    id:27,
+    id: 27,
     title: "GLTF to JSX",
     des: "Export your GLTF file into JSX code, then use it in your projects",
     img: "gltf.png",
-
     link: "https://gltf.pmnd.rs/",
   },
   {
-    id:28,
+    id: 28,
     title: "Godly",
     des: "Astronomically good web design inspiration from all over the internet",
     img: "godly.png",
-
     link: "https://godly.website/",
   },
   {
@@ -237,31 +201,27 @@ export const projects = [
     title: "Growth.design",
     des: "See exactly how companies like Tinder, Airbnb, Trello, Uber and Tesla design products that people love.",
     img: "growth.png",
-
     link: "https://growth.design/case-studies",
   },
   {
-    id:30,
+    id: 30,
     title: "Lighthouse",
     des: "Lighthouse is an open-source, automated tool for improving the performance, quality, and correctness of your web apps.",
     img: "lighthouse.png",
-
     link: "https://chromewebstore.google.com/detail/lighthouse/blipmdconlkpinefehnmjammfjpmpbjk",
   },
   {
-    id:31,
+    id: 31,
     title: "Microcopy",
     des: "Search micro UX copy text: slogans, headlines, notifications, CTA, error messages, email, account preferences, and much more.",
     img: "micro.png",
-
     link: "https://www.microcopy.me/",
   },
   {
-    id:32,
+    id: 32,
     title: "Minimal.Gallery",
     des: "For the love of beautiful, clean and functional websites for your projects",
     img: "minimal.png",
-
     link: "https://minimal.gallery/",
   },
 ];
