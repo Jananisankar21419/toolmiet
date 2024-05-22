7f10f6a8617240e86aacec26daa81a52986ba4b1
Janani <75440572+Jananisankar21419@users.noreply.github.com>
2024-05-22 22:49:36 +0530

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -96,4 +96,172 @@ export const projects = [
 
     link: "https://uploadthing.com/",
   },
+   {
+    id: 12,
+    title: "UploadThing",
+    des: "A simple file uploader for your web app especially for Nextjs Developers",
+    img: "upload.png",
+
+    link: "https://uploadthing.com/",
+  },
+   {
+    id: 13,
+    title: "Adobe Fonts",
+    des: "Adobe Fonts partners with the world’s leading type foundries to bring thousands of fonts to designers every day.",
+    img: "adobe.png",
+
+    link: "https://fonts.adobe.com/",
+  },
+  {
+    id: 14,
+    title: "Art Directed Mockups",
+    des: "Art Directed is a design resources company that creates thoughtful designs",
+    img: "art.png",
+
+    link: "https://artdirected.design/",
+  },
+  {
+    id:15,
+    title: "Atipo Foundry",
+    des: "An independent digital typeface foundry and graphic design studio.",
+    img: "atipo.png",
+
+    link: "https://www.atipofoundry.com/",
+  },
+  {
+    id: 16,
+    title: "Awwwards",
+    des: "Awwwards showcase and award websites that demonstrate exceptional design, innovation, and user experience.",
+    img: "awards.png",
+
+    link: "https://www.awwwards.com/",
+  },
+  {
+    id: 17,
+    title: "Blender",
+    des: "Blender is a free and open-source 3D computer graphics software tool set used for creating animated films, visual effects",
+    img: "blender.png",
+
+    link: "https://www.blender.org/",
+  },
+  {
+    id: 18,
+    title: "Codrops",
+    des: "Keeping web professionals inspired & up-to-date since 2009",
+    img: "codrops.png",
+
+    link: "https://tympanus.net/codrops/",
+  },
+  {
+    id: 19,
+    title: "Curated.design",
+    des: "Unleash your creativity with out-of-this-world web design inspiration from across the web",
+    img: "curated.png",
+
+    link: "https://www.curated.design/",
+  },
+  {
+    id: 20,
+    title: "Dark Mode Design",
+    des: "Dark Mode Design showcases beautifully designed and inspiring dark mode websites. ",
+    img: "darkmode.png",
+
+    link: "https://www.darkmodedesign.com/",
+  },
+  {
+    id:21,
+    title: "Doing Cool Stuff",
+    des: "A directory of brand, motion and digital product design studios doing cool stuff.",
+    img: "dcs.png",
+
+    link: "https://www.doingcoolstuff.xyz/",
+  },
+  {
+    id:22,
+    title: "Easings",
+    des: "Easing functions specify the rate of change of a parameter over time.",
+    img: "easings.png",
+
+    link: "https://easings.net/",
+  },
+  {
+    id:23,
+    title: "Fontshare",
+    des: "Fontshare is a free fonts service from the Indian Type Foundry (ITF), making quality fonts accessible to all.",
+    img: "fontshare.png",
+
+    link: "https://www.fontshare.com/",
+  },
+  {
+    id:24,
+    title: "Footer.design",
+    des: "Footer is a curated gallery of the top website footer inspiration on earth. Find the footers you need and sort by type and style.",
+    img: "footerdesign.png",
+
+    link: "https://www.footer.design/",
+  },
+  {
+    id:25,
+    title: "Frontend Mentor",
+    des: "Improve your front-end skills by building real projects. Solve real-world HTML, CSS and JavaScript challenges ",
+    img: "frontend.png",
+
+    link: "https://www.frontendmentor.io/",
+  },
+  {
+    id:26,
+    title: "Frontend Practice",
+    des: "Take your frontend skills to the next level by recreating real websites from real companies.",
+    img: "frontend-practice",
+
+    link: "https://www.frontendpractice.com/",
+  },
+  {
+    id:27,
+    title: "GLTF to JSX",
+    des: "Export your GLTF file into JSX code, then use it in your projects",
+    img: "gltf.png",
+
+    link: "https://gltf.pmnd.rs/",
+  },
+  {
+    id:28,
+    title: "Godly",
+    des: "Astronomically good web design inspiration from all over the internet",
+    img: "godly.png",
+
+    link: "https://godly.website/",
+  },
+  {
+    id: 29,
+    title: "Growth.design",
+    des: "See exactly how companies like Tinder, Airbnb, Trello, Uber and Tesla design products that people love.",
+    img: "growth.png",
+
+    link: "https://growth.design/case-studies",
+  },
+  {
+    id:30,
+    title: "Lighthouse",
+    des: "Lighthouse is an open-source, automated tool for improving the performance, quality, and correctness of your web apps.",
+    img: "lighthouse.png",
+
+    link: "https://chromewebstore.google.com/detail/lighthouse/blipmdconlkpinefehnmjammfjpmpbjk",
+  },
+  {
+    id:31,
+    title: "Microcopy",
+    des: "Search micro UX copy text: slogans, headlines, notifications, CTA, error messages, email, account preferences, and much more.",
+    img: "micro.png",
+
+    link: "https://www.microcopy.me/",
+  },
+  {
+    id:32,
+    title: "Minimal.Gallery",
+    des: "For the love of beautiful, clean and functional websites for your projects",
+    img: "minimal.png",
+
+    link: "https://minimal.gallery/",
+  },
 ];
