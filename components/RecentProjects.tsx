f0f075f8c9ce87ad9882ab6ceb986e03f23be49c
Janani <75440572+Jananisankar21419@users.noreply.github.com>
2024-05-29 22:44:58 +0530

    Update RecentProjects.tsx

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -35,7 +35,7 @@ const RecentProjects = () => {
                 <img
                   src={item.img}
                   alt="cover"
-                  className="z-10 absolute bottom-0 top-0 object-cover"
+                  className="z-10 absolute bottom-0 object-cover"
                 />
               </div>
 
