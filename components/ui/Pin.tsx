724b35b0d233b322fe7030b412af7096c05ae7a4
Janani <75440572+Jananisankar21419@users.noreply.github.com>
2024-05-29 13:25:24 +0530

    Update Pin.tsx

--- components/ui/Pin.tsx
+++ components/ui/Pin.tsx
@@ -97,7 +97,7 @@ export const PinPerspective = ({
                 opacity: 0,
                 scale: 0,
                 x: "-50%",
-                y: "-50%",
+                y: "0%",
               }}
               animate={{
                 opacity: [0, 1, 0.5, 0],
