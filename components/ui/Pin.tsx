e236ca65bac295a0feb57da4a2f532cc93be9f2d
Janani <75440572+Jananisankar21419@users.noreply.github.com>
2024-05-29 13:28:54 +0530

    Update Pin.tsx

--- components/ui/Pin.tsx
+++ components/ui/Pin.tsx
@@ -130,7 +130,7 @@ export const PinPerspective = ({
                 repeat: Infinity,
                 delay: 2,
               }}
-              className="absolute left-1/2 top-1/2  h-[11.25rem] w-[11.25rem] rounded-[50%] bg-sky-500/[0.08] shadow-[0_8px_16px_rgb(0_0_0/0.4)]"
+              className="absolute left-1/2 top-1/2  h-[11.25rem] w-[11.25rem] object-cover  rounded-[50%] bg-sky-500/[0.08] shadow-[0_8px_16px_rgb(0_0_0/0.4)]"
             ></motion.div>
             <motion.div
               initial={{
