72c040489fb43c8f79d91ecc0e5795861f872dea
Janani <75440572+Jananisankar21419@users.noreply.github.com>
2024-07-27 08:03:30 +0530

    Update MagicButton.tsx

--- components/MagicButton.tsx
+++ components/MagicButton.tsx
@@ -3,10 +3,6 @@ import React from "react";
 /**
  *  UI: border magic from tailwind css btns
  *  Link: https://ui.aceternity.com/components/tailwindcss-buttons
- *
- *  change border radius to rounded-lg
- *  add margin of md:mt-10
- *  remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50
  */
 const MagicButton = ({
   title,
